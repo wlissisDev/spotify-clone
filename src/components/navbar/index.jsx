ab89168b0ef05ebfed6dbbb51b165fc2aea55f6c
wlissisdev <wlissisdev@gmail.com>
2023-04-27 16:30:35 -0300

    navbar completa

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import style from './style.module.css'
 
+import {GrPrevious, GrNext} from 'react-icons/gr'
+
 export default function Navbar() {
   return (
     <div  className={style.container}>
         <div  className={style.buttons}>
-            <span  className={style.btn}>Prev</span>
-            <span  className={style.btn}>Next</span>
+            <span  className={style.btn}><GrPrevious className={style.icon} /></span>
+            <span  className={style.btn}><GrNext className={style.icon}/></span>
         </div>
         <div className={style.items}>
             <a  className={style.btn_inscrever} href="#">Inscrever-se</a>
